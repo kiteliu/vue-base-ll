2edce1262cd189edaf56d3133943002be05b1178
liuliang <liuliang6@100tal.com>
2018-09-20 14:41:21 +0800

    no message

--- config/index.js
+++ config/index.js
@@ -6,7 +6,6 @@ const path = require('path')
 
 module.exports = {
   dev: {
-
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
@@ -19,7 +18,6 @@ module.exports = {
         }
       }
     },
-
     // Various Dev Server settings
     host: '0.0.0.0', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
